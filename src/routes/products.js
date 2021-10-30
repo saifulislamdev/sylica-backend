77e82f8e93695e410c75383ecd9970fd45f267eb
Mhshohag2256 <59985721+Mhshohag2256@users.noreply.github.com>
2021-10-30 08:39:57 -0400

    getProducts endpoint to get all the list of prodcut getProductByID to get a prodcut by id. Endpoints created

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import createProduct from './controllers/productsControllers/createProduct';
+import createProduct from "./controllers/productsControllers/createProduct";
+import getProducts from "./controllers/productsControllers/getProducts";
+import getProductByID from "./controllers/productsControllers/getProductByID";
 
 const router = Router();
 
-router.post('/', createProduct);
-
+router.post("/", createProduct);
+router.get("/getProducts", getProducts);
+router.get("/:productID", getProductByID);
 export default router;
