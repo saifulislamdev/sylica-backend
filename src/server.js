75e18bc840ac00f819d601ad7092ed7515e4cd1c
Saiful Islam <nysaifulislam@gmail.com>
2021-10-30 15:41:03 -0400

    backend CI/Cd completed

--- src/server.js
+++ src/server.js
@@ -22,4 +22,4 @@ app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
